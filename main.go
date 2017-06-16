0b9d027351e7356dc7bae2ee2e8f561c3da7fa13
Son Luong Ngoc <sluongng@gmail.com>
2017-06-16 15:26:28 +0700

    add a few files

--- main.go
+++ main.go
@@ -8,7 +8,30 @@ import (
 	"golang.org/x/net/context"
 
 	httptransport "github.com/go-kit/kit/transport/http"
+	"strings"
 )
+
+// StringService provides operations on strings.
+type StringService interface {
+	Uppercase(string) (string, error)
+	Count(string) int
+}
+
+type stringService struct {}
+
+func (stringService) Uppercase(s string) (string, error) {
+	if s == "" {
+		return "", ErrEmpty
+	}
+	return strings.ToUpper(s), nil
+}
+
+func (stringService) Count(s string) int {
+	return len(s)
+}
+
+// ErrEmpty is returned when input string is empty
+var ErrEmpty = errors.New("Empty string")
 
 func main() {
 	src := stringService{}
