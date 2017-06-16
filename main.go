075a3c13cc9c9c136185e22a9a18bc2a4afa9072
Son Luong Ngoc <sluongng@gmail.com>
2017-06-16 16:24:23 +0700

    update dependencies

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
+	"strings"
 
 	"golang.org/x/net/context"
 
+	"github.com/go-kit/kit/endpoint"
 	httptransport "github.com/go-kit/kit/transport/http"
-	"strings"
 )
 
 // StringService provides operations on strings.
@@ -17,7 +19,7 @@ type StringService interface {
 	Count(string) int
 }
 
-type stringService struct {}
+type stringService struct{}
 
 func (stringService) Uppercase(s string) (string, error) {
 	if s == "" {
@@ -29,9 +31,6 @@ func (stringService) Uppercase(s string) (string, error) {
 func (stringService) Count(s string) int {
 	return len(s)
 }
-
-// ErrEmpty is returned when input string is empty
-var ErrEmpty = errors.New("Empty string")
 
 func main() {
 	src := stringService{}
@@ -53,6 +52,26 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+func makeUppercaseEndpoint(svc StringService) endpoint.Endpoint {
+	return func(ctx context.Context, request interfaceP{}) (interface{}, error) {
+		req := request.(uppercaseRequest)
+		v, err := svc.Uppercase(req.S)
+
+		if err != nil {
+			return uppercaseResponse{v, err.Error()}, nil
+		}
+		return uppercaseResponse{v, ""}, nil
+	}
+}
+
+func makeCountEndpoint(svc StringService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(countRequest)
+		v := svc.Count(req.S)
+		return countResponse{v}, nil
+	}
+}
+
 func decodeUppercaseRequest(_ context.Context, r *http.Request) (interface{}, error) {
 	var request uppercaseRequest
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
@@ -72,3 +91,23 @@ func decodeCountRequest(_ context.Context, r *http.Request) (interface{}, error)
 func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
 	return json.NewEncoder(w).Encode(response)
 }
+
+type uppercaseRequest struct {
+	S string `json:"s"`
+}
+
+type uppercaseResponse struct {
+	V   string `json:"v"`
+	Err string `json:"err,omitempty"` // errors don't JSON-marshal, so we use a string
+}
+
+type countRequest struct {
+	S string `json:"s"`
+}
+
+type countResponse struct {
+	V int `json:"v"`
+}
+
+// ErrEmpty is returned when input string is empty
+var ErrEmpty = errors.New("Empty string")
